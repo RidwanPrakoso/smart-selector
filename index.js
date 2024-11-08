f9df5d2b2631e853ec8819c4b2c2b3884b5ae9c9
RidwanPrakoso <159103648+RidwanPrakoso@users.noreply.github.com>
2024-11-08 16:47:57 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,418 @@
+const laptopData = {
+    "Ideapad Slim 1": {
+        brand: "LENOVO Ideapad Slim 1",
+        role: "Pelajar",
+        segment: "Office",
+        material: "Full ABS Plastic",
+        keyboardBacklight: "NON LED",
+        processor: "Celeron N4020",
+        gpu: "Intel UHD",
+        panel: "TN",
+        storage: "",
+        displaySize: "14\"",
+        displaySpecs: "1366x768, 220 nits, 45% NTSC, 60Hz",
+        note: "",
+        considerations: "",
+        basePrice: 3500000 // Harga dasar
+    },
+    "V14 G3 IAP": {
+        brand: "LENOVO V14 G3 IAP",
+        role: "Pelajar",
+        segment: "Office, Coding",
+        material: "Full ABS Plastic",
+        keyboardBacklight: "NON LED",
+        processor: "Core i3 1215U",
+        gpu: "Intel UHD",
+        panel: "TN",
+        storage: "",
+        displaySize: "14\"",
+        displaySpecs: "1920x1080p, 250 nits, 45% NTSC, 60Hz",
+        note: "",
+        considerations: "",
+        basePrice: 4500000 // Harga dasar
+    },
+    "Ideapad Slim 3": {
+        brand: "LENOVO Ideapad Slim 3",
+        role: "Pelajar, Mahasiswa",
+        segment: "Coding, Office",
+        material: "Full ABS Plastic",
+        keyboardBacklight: "NON LED",
+        processor: "Core i3 1215U",
+        gpu: "Intel UHD",
+        panel: "TN",
+        storage: "",
+        displaySize: "14\"",
+        displaySpecs: "1920x1080p, 250 nits, 45% NTSC, 60Hz",
+        note: "",
+        considerations: "",
+        basePrice: 5590000
+    },
+    "V14 G4 RYZEN 5": {
+        brand: "LENOVO V14 G4 RYZEN 5",
+        role: "Mahasiswa, Karyawan",
+        segment: "Coding, Office, Multimedia",
+        material: "Full ABS Plastic",
+        keyboardBacklight: "NON LED",
+        processor: "Ryzen 5 7430U",
+        gpu: "AMD Integrated",
+        panel: "IPS",
+        storage: "",
+        displaySize: "14\"",
+        displaySpecs: "1920x1080p, 300 nits, 45% NTSC, 60Hz",
+        note: "",
+        considerations: "",
+        basePrice: 7549000
+    },
+    "Ideapad Slim 3 14": {
+        brand: "LENOVO Ideapad Slim 3 14",
+        role: "Mahasiswa, Karyawan",
+        segment: "Coding, Office, Multimedia",
+        material: "Full ABS Plastic",
+        keyboardBacklight: "White LED",
+        processor: "Core i5 12450H",
+        gpu: "Intel UHD",
+        panel: "IPS",
+        storage: "",
+        displaySize: "14\"",
+        displaySpecs: "1920x1080p, 300 nits, 45% NTSC, 60Hz",
+        note: "Sudah mendukung Full Function Type C\nSudah menggunakan DDR5",
+        considerations: "",
+        basePrice: 8279000
+    },
+    "Ideapad Duet 3i 11 Touch": {
+        brand: "LENOVO Ideapad Duet 3i 11 Touch",
+        role: "Karyawan, Mahasiswa, Profesional",
+        segment: "Outdoor, Office, Coding, Multimedia",
+        material: "Full ABS Plastic",
+        keyboardBacklight: "NON LED",
+        processor: "Celeron N200",
+        gpu: "Intel UHD",
+        panel: "IPS",
+        storage: "",
+        displaySize: "11.5\"",
+        displaySpecs: "2000x1200p, 400 nits, 97.5% DCI-P3, 60Hz, Touch Screen",
+        note: "Laptop yang lebih cocok untuk penggunaan entertainment:\n- Menggunakan DDR5\n- Layar Sentuh, Akurasi warna bagus, 2K dan terang untuk dibawa keluar rumah",
+        considerations: "Masih menggunakan Celeron",
+        basePrice: 8999000
+    },
+    "IDEAPAD FLEX 5 14": {
+        brand: "LENOVO IDEAPAD FLEX 5 14",
+        role: "Karyawan, Profesional, Mahasiswa",
+        segment: "Multimedia, Outdoor, Office, Editing, Coding",
+        material: "Full Aluminium",
+        keyboardBacklight: "White LED",
+        processor: "Core i5 1315U",
+        gpu: "Intel UHD",
+        panel: "IPS",
+        storage: "",
+        displaySize: "14\"",
+        displaySpecs: "1920x1200p, 300 nits, 45% NTSC, 60Hz, Touch Screen",
+        note: "",
+        considerations: "",
+        basePrice: 10249000
+    },
+    "LOQ 15 RTX 2050": {
+        brand: "LENOVO LOQ 15 RTX 2050",
+        role: "Karyawan, Profesional, Mahasiswa",
+        segment: "3D / Render & Modeling, Coding, Editing, Office, Gaming, Multimedia",
+        material: "Full ABS Plastic",
+        keyboardBacklight: "NON LED",
+        processor: "Core i5 12450H",
+        gpu: "RTX 2050 4GB",
+        panel: "IPS",
+        storage: "",
+        displaySize: "15.6\"",
+        displaySpecs: "1920x1080p, 300 nits, 100% SRGB, 144Hz",
+        note: "",
+        considerations: "",
+        basePrice: 10749000
+    },
+    "Ideapad Slim 5 Light 14": {
+        brand: "LENOVO Ideapad Slim 5 Light 14",
+        role: "Karyawan, Profesional, Mahasiswa",
+        segment: "3D / Render & Modeling, Coding, Editing, Office, Gaming, Multimedia",
+        material: "Aluminium (Top) ABS (Bottom)",
+        keyboardBacklight: "White LED",
+        processor: "Ryzen 7 7730U",
+        gpu: "AMD Integrated",
+        panel: "IPS",
+        storage: "",
+        displaySize: "15.6\"",
+        displaySpecs: "1920x1080p, 300 nits, 100% SRGB, 60Hz",
+        note: "",
+        considerations: "",
+        basePrice: 10949000
+    },
+    "Ideapad Slim 5": {
+        brand: "LENOVO Ideapad Slim 5",
+        role: "Karyawan, Profesional, Mahasiswa",
+        segment: "Editing, Multimedia",
+        material: "Full Aluminium",
+        keyboardBacklight: "White LED",
+        processor: "Ryzen 7 7730U",
+        gpu: "AMD Integrated",
+        panel: "OLED",
+        storage: "",
+        displaySize: "14\"",
+        displaySpecs: "1920x1200p, 400 nits, 100% DCI-P3, 60Hz",
+        note: "",
+        considerations: "",
+        basePrice: 11299000
+    },
+    "Ideapad Slim 5 (Gaming)": {
+        brand: "LENOVO Ideapad Slim 5 (Gaming)",
+        role: "Karyawan, Profesional, Mahasiswa",
+        segment: "Editing, Gaming, Multimedia",
+        material: "Full Aluminium",
+        keyboardBacklight: "White LED",
+        processor: "Core ULTRA 5 125H",
+        gpu: "Arc 7X",
+        panel: "OLED",
+        storage: "",
+        displaySize: "14\"",
+        displaySpecs: "1920x1200p, 400 nits, 100% DCI-P3, 60Hz",
+        note: "",
+        considerations: "",
+        basePrice: 12199000
+    },
+    "LOQ 15 RTX 3050": {
+        brand: "LENOVO LOQ 15 RTX 3050",
+        role: "Mahasiswa, Karyawan, Profesional",
+        segment: "3D / Render & Modeling, Coding, Editing, Office, Gaming, Multimedia",
+        material: "Full ABS Plastic",
+        keyboardBacklight: "White LED",
+        processor: "Core i5 12450H",
+        gpu: "RTX 3050 6GB",
+        panel: "IPS",
+        storage: "",
+        displaySize: "15.6\"",
+        displaySpecs: "1920x1080p, 300 nits, 100% SRGB, 60Hz",
+        note: "",
+        considerations: "",
+        basePrice: 13490000
+    }
+};
+
+// Harga tambahan berdasarkan RAM dan penyimpanan
+const additionalPrice = {
+    "8GB DDR4": 500000,
+    "16GB DDR4": 2000000,
+    "8GB LPDDR5": 500000,
+    "16GB LPDDR5": 1300000,
+    "12GB DDR5": 1500000,
+    "16GB DDR5": 2300000,
+    "8GB LPDDR5X": 1700000,
+    "16GB LPDDR5X": 2500000, 
+    "8GB LPDDR4X": 1600000,
+    "16GB LPDDR4X": 2600000,
+    "NVME 3.0 256GB": 500000,
+    "512GB NVME 3.0": 1500000,
+};
+
+
+
+
+
+// Fungsi untuk memperbarui daftar model berdasarkan peran
+function updateModels() {
+    const role = document.getElementById('role').value;
+    const modelSelect = document.getElementById('model');
+    
+    // Bersihkan opsi pilihan sebelumnya
+    modelSelect.innerHTML = '<option value="">Select Model</option>';
+    
+    let availableModels = [];
+
+    if (role === "Pelajar") {
+        availableModels = ["Ideapad Slim 1", "V14 G3 IAP"];
+    } else if (role === "Pelajar, Mahasiswa") {
+        availableModels = ["Ideapad Slim 3"];
+    } else if (role === "Mahasiswa, Karyawan") {
+        availableModels = ["V14 G4 RYZEN 5", "Ideapad Slim 3 14"];
+    } else if (role === "Profesional, Mahasiswa, Karyawan") {
+        availableModels = [
+            "Ideapad Duet 3i 11 Touch", "IDEAPAD FLEX 5 14", 
+            "LOQ 15 RTX 2050", "Ideapad Slim 5 Light 14", 
+            "Ideapad Slim 5","Ideapad Slim 5 (Gaming)", "LOQ 15 RTX 3050"
+        ];
+    }
+
+    availableModels.forEach(model => {
+        modelSelect.innerHTML += `<option value="${model}">${model}</option>`;
+    });
+}
+
+// Fungsi untuk memperbarui pilihan RAM berdasarkan model yang dipilih
+function updateRAMOptions() {
+    const model = document.getElementById('model').value;
+    const ramSelect = document.getElementById('ram');
+
+    // Bersihkan opsi RAM sebelumnya
+    ramSelect.innerHTML = '<option value="">Select RAM</option>';
+
+    let validRAMOptions = [];
+
+    // Tentukan opsi RAM berdasarkan model yang dipilih
+    switch(model) {
+        case "Ideapad Slim 1":
+        case "V14 G3 IAP":
+            validRAMOptions = ["8GB DDR4", "16GB DDR4"];
+            break;
+        case "Ideapad Slim 3":
+            validRAMOptions = ["8GB DDR4", "16GB DDR4"];
+            break;
+        case "V14 G4 RYZEN 5":
+            validRAMOptions = ["8GB DDR4", "16GB DDR4"];
+            break;
+        case "Ideapad Slim 3 14":
+            validRAMOptions = ["8GB LPDDR5", "16GB LPDDR5"];
+            break;
+        case "IDEAPAD FLEX 5 14":
+            validRAMOptions = ["8GB LPDDR4X", "16GB LPDDR4X"];
+            break;
+        case "LOQ 15 RTX 3050":
+            validRAMOptions = ["12GB DDR5", "16GB DDR5"];
+            break;
+        case "Ideapad Duet 3i 11 Touch":
+            validRAMOptions = ["8GB LPDDR5", "16GB LPDDR5"];
+            break;
+        case "LOQ 15 RTX 2050":
+            validRAMOptions = ["12GB DDR5", "16GB DDR5"];
+            break;
+        case "Ideapad Slim 5 Light 14":
+            validRAMOptions = ["8GB DDR4", "16GB DDR4"];
+            break;
+        case "Ideapad Slim 5":
+            validRAMOptions = ["8GB LPDDR5X", "16GB LPDDR5X"];
+            break;
+        case "Ideapad Slim 5 (Gaming)":
+            validRAMOptions = ["8GB LPDDR5X", "16GB LPDDR5X"];
+            break;
+        default:
+            validRAMOptions = [];
+    }
+
+    // Tambahkan opsi RAM yang valid ke dropdown
+    validRAMOptions.forEach(option => {
+        ramSelect.innerHTML += `<option value="${option}">${option}</option>`;
+    });
+}
+
+
+
+
+// Fungsi untuk memperbarui pilihan segmen berdasarkan model dan role
+function updateSegmentOptions() {
+    const role = document.getElementById("role").value;
+    const model = document.getElementById("model").value;
+    const segmentSelect = document.getElementById("segment");
+
+    segmentSelect.innerHTML = '<option value="" disabled selected>-Pilih Segmen-</option>';
+    let validSegments = [];
+    switch (role) {
+        case "Pelajar":
+            if (model === "Ideapad Slim 1") {
+                validSegments = ["Office"];
+            } else if (model === "V14 G3 IAP") {
+                validSegments = ["Coding, Office"];
+            }
+            break;
+    
+        case "Pelajar, Mahasiswa":
+            if (model === "Ideapad Slim 3") {
+                validSegments = ["Coding, Office"];
+            }
+            break;
+    
+        case "Mahasiswa, Karyawan":
+            if (model === "V14 G4 RYZEN 5" || model === "Ideapad Slim 3 14") {
+                validSegments = ["Coding, Office, Multimedia"];
+            }
+            break;
+    
+            case "Profesional, Mahasiswa, Karyawan":
+                if (model === "LOQ 15 RTX 2050" || model === "Ideapad Slim 5 Light 14" || model === "LOQ 15 RTX 3050") {
+                    validSegments = ["3D / Render & Modeling, Coding, Editing, Office, Gaming, Multimedia"];
+                } else if (model === "Ideapad Duet 3i 11 Touch") {
+                    validSegments = ["Outdoor, Office, Coding, Multimedia"];
+                } else if (model === "IDEAPAD FLEX 5 14") {
+                    validSegments = ["Multimedia, Outdoor, Office, Editing, Coding"];
+                } else if (model === "Ideapad Slim 5") {
+                    validSegments = ["Editing, Multimedia"];
+                } else if (model === "Ideapad Slim 5 (Gaming)") {  // Nama sesuai dengan data di laptopData
+                    validSegments = ["Editing, Multimedia, Gaming"];
+                }
+                break;
+            
+    }
+    
+    // Tambahkan segmen yang valid ke dropdown jika validSegments tidak kosong
+   
+    
+    validSegments.forEach(segment => {
+        segmentSelect.innerHTML += `<option value="${segment}">${segment}</option>`;
+    });
+}
+
+
+    
+// Fungsi untuk menampilkan spesifikasi laptop dan total harga
+function displaySpecifications() {
+    const role = document.getElementById('role').value;
+    const model = document.getElementById('model').value;
+    const segment = document.getElementById('segment').value;
+    const ram = document.getElementById('ram').value;
+    const storage = document.getElementById('storage').value;
+
+// Periksa apakah semua opsi telah dipilih
+    if (!role || !model || !segment || !ram || !storage) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Mohon pilih semua opsi sebelum menampilkan spesifikasi.'
+        });
+        return;
+    }
+ // Ambil data laptop dan harga tambahan
+    const laptop = laptopData[model];
+    const ramPrice = additionalPrice[ram] || 0;
+    const storagePrice = additionalPrice[storage] || 0;
+    const totalPrice = laptop.basePrice + ramPrice + storagePrice;
+
+// Tampilkan spesifikasi dan total harga
+    Swal.fire({
+        title: 'Spesifikasi Laptop',
+        html: `
+            <div style="text-align: left;">
+                <strong>Brand:</strong> ${laptop.brand}<br>
+                <strong>Role:</strong> ${laptop.role}<br>
+                <strong>Segment:</strong> ${laptop.segment}<br>
+                <strong>Processor:</strong> ${laptop.processor}<br>
+                <strong>GPU:</strong> ${laptop.gpu}<br>
+                <strong>Panel:</strong> ${laptop.panel}<br>
+                <strong>RAM:</strong> ${ram}<br>
+                <strong>Storage:</strong> ${storage}<br>
+                <strong>Display Size:</strong> ${laptop.displaySize}<br>
+                <strong>Display Specs:</strong> ${laptop.displaySpecs}<br>
+                <strong>Note:</strong> ${laptop.note || 'N/A'}<br>
+                <strong>Considerations:</strong> ${laptop.considerations || 'N/A'}<br>
+                <strong>Total Price:</strong> Rp ${totalPrice.toLocaleString()}<br>
+            </div>
+        `,
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonText: 'Selesai',
+        cancelButtonText: 'Kembali',
+        confirmButtonColor: '#28a745',
+        cancelButtonColor: '#d33',
+        background: '#f8f9fa',
+        width: '600px',
+        padding: '20px',
+        customClass: {
+            title: 'font-weight-bold',
+            htmlContainer: 'text-start'
+        },
+        didOpen: () => Swal.showLoading(),
+        willClose: () => Swal.close()
+    });
+}
